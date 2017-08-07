2e5af78a3a256c7ac2c1d3d0e04fd1627b59d74e
elfiservice <elfiservice@hotmail.com>
2017-08-07 15:44:14 -0300

    feat: clean entry value and show result

--- js/script.js
+++ js/script.js
@@ -1,3 +1,50 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+    var elemtOutValueContainer = $('#outValueContainer');
+    var btnGo = $('#go-btn');
+
+    var model = {
+        value: "",
+        setValue: function(value){
+            model.value = value;
+        },
+        getValue: function(){
+            return model.value;
+        }
+    }
+
+    var octpus = {
+        init: function(){
+
+            view.init();
+        },
+        setCurrentValue: function(outValue){
+            model.setValue(outValue);
+        },
+        getCurrentValue: function(){
+            return model.getValue();
+        }
+    };
+
+    var view = {
+        init: function() {
+            btnGo.click(function(){
+                var outValueContente = $('.out-value-content');
+                var entreyValue = $('#entryValue').val();
+                var outValue =  entreyValue.replace(/[^\d]+/g,'');
+                if(outValueContente){
+                    outValueContente.remove();
+                }
+
+                view.render(outValue);
+            });
+        },
+
+        render: function(outValue){
+            octpus.setCurrentValue(outValue);
+            elemtOutValueContainer.append('<div class="out-value-content">' + octpus.getCurrentValue() + '</div>');
+        }
+    };
+
+octpus.init();
 });
